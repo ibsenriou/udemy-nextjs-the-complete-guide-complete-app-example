e594b9682e6df023e83c0af8efa87aa31e58c6b6
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-08-25 10:45:43 -0300

    Add Next Config

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -23,16 +23,16 @@ async function handler(req, res) {
 
     let client;
 
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.itypnoi.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        'mongodb+srv://admin:admin@cluster0.itypnoi.mongodb.net/my-site?retryWrites=true&w=majority'
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: 'Could not connect to database.' });
       return;
     }
 
-    const db = client.db('my-site');
+    const db = client.db();
 
     try {
       const result = await db.collection('messages').insertOne(newMessage);
